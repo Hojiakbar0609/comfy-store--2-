95b625fe5af1319b85f4b804e1793151776a092d
haydarovhojiakbarjsonjson* <haydarovhojiakbar97@gmail.com>
2023-09-18 12:00:08 +0500

    comfy-store

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+// rrd
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// pages and layoutes
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+
+// components
+import { ErrorElement } from "./components";
+
+// loaders
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleProductLoader } from "./pages/SingleProduct";
+function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Landing />,
+          loader: landingLoader,
+          errorElement: <ErrorElement/>
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "orders",
+          element: <Orders />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+          errorElement: <ErrorElement/>
+        },
+        {
+          path: "products/:id",
+          element: <SingleProduct />,
+          loader: singleProductLoader,
+          errorElement: <ErrorElement/>
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+        {
+          path: "checkout",
+          element: <Checkout />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+      errorElement: <Error />,
+    },
+  ]);
+
+  return (<RouterProvider router={routes} />);
+}
+
+export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,25 @@
+import Hero from './Hero'
+import { customFetch } from '../utils'
+// components
+import { FeaturedProducts } from '../components'
+
+// url
+const url = '/products?featured=true'
+
+// loader
+export const loader = async () => {
+  const request = await customFetch(url)
+  const products = request.data.data
+  return { products }
+}
+
+function Landing() {
+  return (
+    <>
+      <Hero/>
+      <FeaturedProducts/>
+    </>
+  )
+}
+
+export default Landing
