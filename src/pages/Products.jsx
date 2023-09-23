1a9b4eb54b389c0556d105108ade670ca241a2a7
haydarovhojiakbarjsonjson* <haydarovhojiakbar97@gmail.com>
2023-09-23 11:06:55 +0500

    filter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,18 +2,24 @@ import Filter from "../components/Filter"
 
 const url = '/products'
 export const loader = async ({ request }) => {
-  const response = await customFetch(url)
-  const products = response.data.data
-  const meta = response.data.meta
-  return { products, meta }
-}
+  
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries()
+  ])
+  const response = await customFetch(url, {
+    params
+  });
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, params, meta };
+};
 
 import { ProductContainer, PaginationContainer } from "../components"
 
 import { customFetch } from "../utils"
 function Products() {
   return (
-    <div className="align-elements">
+    <div className="align-elements py-20">
       <Filter/>
       <ProductContainer/>
       <PaginationContainer/>
